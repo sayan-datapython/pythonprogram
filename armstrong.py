fbe116192dcd18893e6eee2391207cf442d3c509
sayan-datapython <72145939+sayan-datapython@users.noreply.github.com>
2020-09-30 23:17:02 +0530

    Update armstrong.py
    Your code was little bit wrong,now it is perfect

--- armstrong.py
+++ armstrong.py
@@ -1,9 +1,11 @@
 n=int(input("enter the number"))
 i=0
+r=0
 temp=n
-while n>0:
+length=len(str(n))
+while n!=0:
 	r=n%10
-	i=i+(r**3)
+	i=i+(r**length)
 	n=n//10
 if temp==i:
 	print('it is armstrong number')
